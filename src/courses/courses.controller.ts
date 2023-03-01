96bd510adbd8d66a4b88e608366bea2e5b8b7263
Carlos Jaime <jaime_andrek@hotmail.com>
2023-03-01 20:22:51 -0300

    Juntando o controller com o service

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,40 +1,33 @@
 import { Body, Controller, Get, HttpCode, Param, Post, HttpStatus, Res, Patch, Delete } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { Course } from './entities/course.entity';
 
 @Controller('courses') //posso definir um prefix ou não
 export class CoursesController {
-    constructor(private readonly coursesService: CoursesService){
-
-    }
+    constructor(private readonly coursesService: CoursesService){}
     
     @Get('list')
-    findAll(@Res() response): string {
-        return response.status(200).json("Listagem de cursos") // não é a melhor forma de retornar o HTTP CODE
+    findAll(): Course[] {
+        return this.coursesService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string): string {
-        return `Curso numero #${id}`
+    findOne(@Param('id') id: string): Course {
+        return this.coursesService.findOne(id);
     }
 
     @Post()
-    @HttpCode(HttpStatus.NO_CONTENT) 
     create(@Body() body) {
-        return body
+        return this.coursesService.create(body);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body): object {
-        return {
-            message: `Atualizando o campo ${id}`,
-            body
-        } 
+    update(@Param('id') id: string, @Body() body) {
+        return this.coursesService.update(id, body);
     }
     
     @Delete(':id')
-    delete(@Param('id') id: string): object {
-        return {
-            message: `Deletando o registro ${id}`
-        } 
+    delete(@Param('id') id: string) {
+        return this.coursesService.remove(id);
     }
 }
