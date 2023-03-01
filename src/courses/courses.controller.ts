2ce35a52fe00baf1cde016e559ec303428e0ab79
Carlos Jaime <jaime_andrek@hotmail.com>
2023-03-01 17:21:40 -0300

    controller pegando parametros

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses') //posso definir um prefix ou não
 export class CoursesController {
@@ -6,5 +6,10 @@ export class CoursesController {
     @Get('list')
     findAll(): string {
         return "Listagem de recursos"
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string): string {
+        return `Curso numero #${id}`
     }
 }
