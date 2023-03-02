30c9aadcf7c18950259281145bbd671e063c037a
Carlos Jaime <jaime_andrek@hotmail.com>
2023-03-02 08:26:30 -0300

    conceito de DTO

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Get, HttpCode, Param, Post, HttpStatus, Res, Patch, Delete } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { Course } from './entities/course.entity';
+import { CreateCourseDto } from './dto/create-course.dto/create-course.dto';
+import { UpdateCourseDto } from './dto/create-course.dto/update-course.dto';
 
 @Controller('courses') //posso definir um prefix ou não
 export class CoursesController {
@@ -17,13 +19,13 @@ export class CoursesController {
     }
 
     @Post()
-    create(@Body() body) {
-        return this.coursesService.create(body);
+    create(@Body() createdCourseDto: CreateCourseDto) {
+        return this.coursesService.create(createdCourseDto);
     }
 
     @Patch(':id')
-    update(@Param('id') id: string, @Body() body) {
-        return this.coursesService.update(id, body);
+    update(@Param('id') id: string, @Body() updateCourseDto: UpdateCourseDto) {
+        return this.coursesService.update(id, updateCourseDto);
     }
     
     @Delete(':id')
