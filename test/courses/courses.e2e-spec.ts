ebe6e7a44c3b672660fe70a0dc5a00765d466ea3
Carlos Jaime <jaime_andrek@hotmail.com>
2023-03-13 09:45:24 -0300

    exemplo de test end2end courseService

--- test/courses/courses.e2e-spec.ts
+++ test/courses/courses.e2e-spec.ts
@@ -1,17 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { CoursesModule } from 'src/courses/courses.module';
+import { CoursesModule } from '../../src/courses/courses.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CreateCourseDto } from 'src/courses/dto/create-course.dto/create-course.dto';
 
 describe('Courses /courses', () => {
   let app: INestApplication;
 
+  const course: CreateCourseDto = {
+    name: 'Nestjs com TypeORM',
+    description: 'Criando apis restful com nestj',
+    tags: ['nestjs', 'javascript']
+
+  }
+
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [CoursesModule],
+      imports: [CoursesModule, TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'root',
+        database: 'test',
+        autoLoadEntities: true,
+        synchronize: true
+      })],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true, //verifica se existe um dado fora do esperado e egnora ele
+      forbidNonWhitelisted: true, // não permite enviar dados que não foram mapeado
+      transform: true //defini o tipo de valor que chega como valor do body 
+    }))
+
+
     await app.init();
   });
 
@@ -19,5 +45,10 @@ describe('Courses /courses', () => {
     await app.close();
   })
 
-  it.todo('Create POST/courses')
+  it('Create POST/courses', () => {
+    return request(app.getHttpServer())
+    .post('/courses')
+    .send(course)
+    .expect(HttpStatus.CREATED)
+  })
 });
